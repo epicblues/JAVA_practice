c270984051cb1648f7be1c5d8c2374b6e743b6d1
김민성 <epicblue@gimminseong-ui-MacBookAir.local>
2021-06-26 19:38:44 +0900

    JAVA practice Sample

---
+++ src/ex1/io/printstream/Program.java
@@ -0,0 +1,33 @@
+package ex1.io.printstream;
+
+public class Program {
+	public static void main(String[] args) {
+
+		int kor1, kor2, kor3;
+		int total;
+		float avg;
+
+		kor1 = 50;
+		kor2 = 60;
+		kor3 = 80;
+
+		total = kor1 + kor2 + kor3;
+		avg = total / 3;
+
+//		------------------ 성적 출력 구분 ---------------------
+		
+		System.out.print("⎧⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎫\n");
+		System.out.print("⎪    성적 출력      ⎪\n");
+		System.out.print("⎪                 ⎪\n");
+		System.out.print("⎩⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎭\n");
+
+		
+		System.out.printf(" 국어1 : %d\n", kor1);
+		System.out.printf(" 국어2 : %d\n", kor2);
+		System.out.printf(" 국어3 : %d\n", kor3);
+		System.out.printf(" 총점 : %d\n", total);
+		System.out.printf(" 평균 : %s\n", avg);
+		System.out.print("⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽⎽");
+
+	}
+}
